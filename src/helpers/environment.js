a33e250bb06cff03e3cd4a52eefbe78913a170ed
William Stergar <stergarwill@gmail.com>
2021-11-30 22:36:37 -0500

    first cryptocollabclient push

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -2,12 +2,12 @@ let APIURL = ""
 
 switch (window.location.hostname) {
     case "localhost" || "127.0.0.1":
-        APIURL = "http://localhost:<5000>"
+        APIURL = "http://localhost:5000"
         break
 
     case "cryptocollabclient.herokuapp.com":
 
-        APIURL = "https://cryptocollabclient.herokuapp.com"
+        APIURL = "https://cryptocollabserver.herokuapp.com"
 }
 
 export default APIURL
